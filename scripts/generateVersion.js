271d041e4e428bd8b3e99dc912bf958aa5340ce5
e-danz <eduard.danziger@t-online.de>
2025-02-11 20:48:20 +0100

    Version has RC part

--- scripts/generateVersion.js
+++ scripts/generateVersion.js
@@ -7,6 +7,14 @@ const version = lastTag.replace('Release-', '');
 
 const commitCount = execSync(`git rev-list --count ${lastTag}..HEAD`).toString().trim();
 
-const fullVersion = `${version}.${commitCount}`;
+const versionParts = version.split('.');
+if (commitCount !== '0' && versionParts.length > 2) {
+    versionParts[2] = (parseInt(versionParts[2], 10) + 1).toString();
+}
+
+const paddedCommitCount = commitCount.padStart(3, '0');
+
+const newVersion = versionParts.join('.');
+const fullVersion = commitCount === '0' ? version : `${newVersion}-RC${paddedCommitCount}`;
 
 writeFileSync('public/version.json', JSON.stringify({ version: fullVersion }));
