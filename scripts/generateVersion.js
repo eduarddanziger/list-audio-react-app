717a714304aa4656337105c8c53c90b6f57a0eba
e-danz <eduard.danziger@t-online.de>
2025-02-19 19:00:43 +0100

    Version is generated in YML

--- scripts/generateVersion.js
+++ scripts/generateVersion.js
@@ -1,20 +1,5 @@
-import { execSync } from 'child_process';
 import { writeFileSync } from 'fs';
 
-const lastTag = execSync('git describe --tags --abbrev=0').toString().trim();
-
-const version = lastTag.replace('Release-', '');
-
-const commitCount = execSync(`git rev-list --count ${lastTag}..HEAD`).toString().trim();
-
-const versionParts = version.split('.');
-if (commitCount !== '0' && versionParts.length > 2) {
-    versionParts[2] = (parseInt(versionParts[2], 10) + 1).toString();
-}
-
-const paddedCommitCount = commitCount.padStart(3, '0');
-
-const newVersion = versionParts.join('.');
-const fullVersion = commitCount === '0' ? version : `${newVersion}-RC${paddedCommitCount}`;
+const fullVersion = process.env.VERSION;
 
 writeFileSync('public/version.json', JSON.stringify({ version: fullVersion }));
