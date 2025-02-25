22c0b4bb76bb7385fdfc88769c68317758dc2dc2
Eduard D. <180659468+eduarddanziger@users.noreply.github.com>
2025-02-25 23:33:34 +0000

    set public port via gh

--- scripts/start-service.sh
+++ scripts/start-service.sh
@@ -3,3 +3,9 @@
 cd /workspaces/list-audio-react-app
 git pull
 dotnet run --project ./DeviceRepoAspNetCore/DeviceRepoAspNetCore.csproj --launch-profile http
+
+# Wait for the service to start
+sleep 5
+
+# Set port visibility to public
+gh codespace ports visibility 5027:public -c $CODESPACE_NAME
