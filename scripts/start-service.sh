610256889e25c0ec9f3ceaeece2ad9c3886fa595
Eduard D. <180659468+eduarddanziger@users.noreply.github.com>
2025-02-25 23:44:57 +0000

    Wait for service to start

--- scripts/start-service.sh
+++ scripts/start-service.sh
@@ -5,7 +5,10 @@ git pull
 dotnet run --project ./DeviceRepoAspNetCore/DeviceRepoAspNetCore.csproj --launch-profile http &
 
 # Wait for the service to start
-sleep 5
+until $(curl --output /dev/null --silent --head --fail http://localhost:5027); do
+  printf '.'
+  sleep 1
+done
 
 # Set port visibility to public
 gh codespace ports visibility 5027:public -c $CODESPACE_NAME
