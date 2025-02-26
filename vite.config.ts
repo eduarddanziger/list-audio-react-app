624758f88a24b3cd91138c3bfbe457179f45ab78
Eduard D. <180659468+eduarddanziger@users.noreply.github.com>
2025-02-26 00:29:35 +0000

    removed usage of UNIVERSAL_PAT

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv';
-
-// Load environment variables from .env file
-dotenv.config();
 
 // https://vite.dev/config/
 export default defineConfig({
