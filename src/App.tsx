8ecce9495c28908bc6a34c7c6b801e9261efb464
eduarddanziger <edanziger@gmx.de>
2025-03-03 23:54:56 +0100

    Clean up

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import NavBar from './components/NavBar';
-import { useEffect } from 'react';
-import { startCodespace } from './startCodespace';
 
 function App() {
-    useEffect(() => {
-        if (process.env.NODE_ENV != 'development') {
-            startCodespace().then(r => console.log(r));
-        }
-    }, []);
-
     return (
         <Router basename="/list-audio-react-app">
             <NavBar />
