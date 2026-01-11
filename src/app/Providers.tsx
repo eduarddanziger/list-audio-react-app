f2e5901f729a37f50a434be8201b609cd2557d7b
danziger-e <eduard.danziger@t-online.de>
2026-01-11 10:32:10 +0100

    Refactor layout and Providers components for improved readability

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,22 +1,26 @@
 'use client';
 
-import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
-import { I18nextProvider } from 'react-i18next';
+import {AppRouterCacheProvider} from '@mui/material-nextjs/v15-appRouter';
+import {I18nextProvider} from 'react-i18next';
 import i18n from '../i18n';
-import { ThemeProviderComponent } from '../contexts/ThemeContext';
+import {ThemeProviderComponent} from '../contexts/ThemeContext';
 import RootClient from './RootClient';
+import React from "react";
 
-export default function Providers({ children, initialMode }: { children: React.ReactNode; initialMode?: 'light' | 'dark' }) {
-  return (
-    <AppRouterCacheProvider>
-      <I18nextProvider i18n={i18n}>
-        <ThemeProviderComponent initialMode={initialMode}>
-          <RootClient>
-            {children}
-          </RootClient>
-        </ThemeProviderComponent>
-      </I18nextProvider>
-    </AppRouterCacheProvider>
-  );
+export default function Providers({children, initialMode}: {
+    children: React.ReactNode;
+    initialMode?: 'light' | 'dark'
+}) {
+    return (
+        <AppRouterCacheProvider>
+            <I18nextProvider i18n={i18n}>
+                <ThemeProviderComponent initialMode={initialMode}>
+                    <RootClient>
+                        {children}
+                    </RootClient>
+                </ThemeProviderComponent>
+            </I18nextProvider>
+        </AppRouterCacheProvider>
+    );
 }
 
